4e81a0443ea331a08f25fa319730d63921b25f00
Emir <emirbalic@gmail.com>
2022-11-14 20:42:37 +0100

    First Commit

---
+++ src/components/JobResults/JobFiltersSidebar/JobFiltersSidebarCheckboxGroup.vue
@@ -0,0 +1,66 @@
+<template>
+  <accordion :header="header">
+    <div class="mt-5">
+      <fieldset>
+        <ul class="flex flex-row flex-wrap">
+          <li v-for="value in uniqueValues" :key="value" class="w-1/2 h-8">
+            <input
+              :id="value"
+              v-model="selectedValues"
+              :value="value"
+              type="checkbox"
+              class="mr-3"
+              :data-test="value"
+              @change="selectValue"
+            />
+            <label :for="value" data-test="value">{{ value }}</label>
+          </li>
+        </ul>
+      </fieldset>
+    </div>
+  </accordion>
+</template>
+
+<script lang="ts">
+import { ref, defineComponent } from "vue";
+import { useStore } from "vuex";
+import { useRouter } from "vue-router";
+
+import { key } from "@/store";
+
+import Accordion from "@/components/Shared/Accordion.vue";
+
+export default defineComponent({
+  name: "JobFiltersSidebarCheckboxGroup",
+  components: {
+    Accordion,
+  },
+  props: {
+    header: {
+      type: String,
+      required: true,
+    },
+    uniqueValues: {
+      type: Set,
+      required: true,
+    },
+    mutation: {
+      type: String,
+      required: true,
+    },
+  },
+  setup(props) {
+    const store = useStore(key);
+    const router = useRouter();
+
+    const selectedValues = ref<string[]>([]);
+
+    const selectValue = () => {
+      store.commit(props.mutation, selectedValues.value);
+      router.push({ name: "JobResults" });
+    };
+
+    return { selectedValues, selectValue };
+  },
+});
+</script>
---
+++ src/store/composables.ts
@@ -0,0 +1,35 @@
+import { computed } from "vue";
+import { useStore } from "vuex";
+
+import {
+  FILTERED_JOBS,
+  UNIQUE_JOB_TYPES,
+  UNIQUE_ORGANIZATIONS,
+  FETCH_JOBS,
+} from "@/store/constants";
+
+import { Job } from "@/api/types";
+
+import { key } from "@/store";
+
+/* GETTERS */
+export const useFilteredJobs = () => {
+  const store = useStore(key);
+  return computed<Job[]>(() => store.getters[FILTERED_JOBS]);
+};
+
+export const useUniqueJobTypes = () => {
+  const store = useStore(key);
+  return computed<Set<string>>(() => store.getters[UNIQUE_JOB_TYPES]);
+};
+
+export const useUniqueOrganizations = () => {
+  const store = useStore(key);
+  return computed<Set<string>>(() => store.getters[UNIQUE_ORGANIZATIONS]);
+};
+
+/* ACTIONS */
+export const useFetchJobsDispatch = () => {
+  const store = useStore(key);
+  store.dispatch(FETCH_JOBS);
+};
